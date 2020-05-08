13064b0b44fe2a4da29092345aa1dd82f73285ef
YFuean <1412666584@qq.com>
2020-05-08 23:23:06 +0800

    day44、45

--- 语法基础/Day43/dict和set经典使用1.py
+++ 语法基础/Day43/dict和set经典使用1.py
@@ -6,13 +6,11 @@ dict和set经典使用1
 d = {'a': 1, 'b': 2}
 d.update({'c': 3, 'd': 4, 'e': 5})
 print(d)
-
 # 2、不存则插入，存在则不插入不更新
 d = {'a': 1, 'b': 2}
 x = d.setdefault('c', 3)
 y = d.setdefault('a', 1)
 print(d)
-
 # 3、字典并集
 
 
@@ -21,7 +19,6 @@ def merge(d1, d2):
 
 
 print(merge({'a': 1, 'b': 2}, {'c': 3}))
-
 # 4、字典差
 
 
@@ -30,7 +27,6 @@ def difference(d1, d2):
 
 
 print(difference({'a': 1, 'b': 2, 'c': 3}, {'b': 2}))
-
 # 5、按键排序
 
 
@@ -39,7 +35,6 @@ def sort_by_key(d):
 
 
 print(sort_by_key({'a': 3, 'b': 1, 'c': 2}))
-
 # 6、按值排序
 
 
